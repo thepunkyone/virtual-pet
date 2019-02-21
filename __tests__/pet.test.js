749c67d77df75f1f74de8203d388b90731b5a8da
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-21 21:19:21 +0000

    added age property and prototype growUp method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,13 +1,18 @@
 const Pet = require('../src/pet');
 
 describe('Pet constructor', () => {
+    const pet = new Pet('Fido');
     it('returns an object', () => {
       expect(new Pet()).toBeInstanceOf(Object);
     });
     it('check that an instance of Pet with a name property has been created', () => {
-        const pet = new Pet('Fido');
-        expect(pet).toEqual({
-            name: 'Fido'
-        });
+        expect(pet.name).toBe('Fido');
+    });
+    it('has a initial age of 0', () => {
+        expect(pet.age).toBe(0);
+    });
+    it ('growUp method adds one year to pet\'s age', () => {
+        pet.growUp();
+        expect(pet.age).toBe(1);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,10 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
 };
+
+Pet.prototype.growUp = function() {
+   this.age ++; 
+}
 
 module.exports = Pet;
