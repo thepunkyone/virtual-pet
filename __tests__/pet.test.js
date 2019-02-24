bb575346d482b60d20d276605e0f3183db5b4988
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-24 15:39:39 +0000

    added hunger and fitness properties to Pet constructor, expanded growUp method to change values of hunger and fitness when called, added a walk method to Pet.prototype, refactored code to use variables instead of magic numbers

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,18 +1,41 @@
 const Pet = require('../src/pet');
 
 describe('Pet constructor', () => {
-    const pet = new Pet('Fido');
+    pet = new Pet('Fido');
+
     it('returns an object', () => {
-      expect(new Pet()).toBeInstanceOf(Object);
+      expect(pet).toBeInstanceOf(Object);
     });
-    it('check that an instance of Pet with a name property has been created', () => {
+    it('check that an instance of Pet with a name property and value has been created', () => {
         expect(pet.name).toBe('Fido');
     });
-    it('has a initial age of 0', () => {
+    it('has an initial age of 0', () => {
         expect(pet.age).toBe(0);
     });
-    it ('growUp method adds one year to pet\'s age', () => {
+    it('has an initial hunger level of 0', () => {
+        expect(pet.hunger).toBe(0);
+    });
+    it('has an initial fitness level of 10', () => {
+        expect(pet.fitness).toBe(10);
+    });
+    it ('growUp method adds one year to pet\'s age, five points to hunger and removes three points from fitness', () => {
         pet.growUp();
         expect(pet.age).toBe(1);
+        expect(pet.hunger).toBe(5);
+        expect(pet.fitness).toBe(7);
+
+        pet.growUp();
+        expect(pet.age).toBe(2);
+        expect(pet.hunger).toBe(10);
+        expect(pet.fitness).toBe(4);
+    });
+    it ('walk method adds four points to pet\'s fitness until a maximum of 10 points is achieved', () => {
+        pet.fitness = 5;
+
+        pet.walk();
+        expect(pet.fitness).toBe(9);
+
+        pet.walk();
+        expect(pet.fitness).toBe(10);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,33 @@
+const initialAge = 0;
+const initialHunger = 0;
+const initialFitness = 10;
+
 function Pet(name) {
     this.name = name;
-    this.age = 0;
+    this.age = initialAge;
+    this.hunger = initialHunger;
+    this.fitness = initialFitness;
 };
 
 Pet.prototype.growUp = function() {
+    const hungerIncrement = 5;
+    const fitnessDecrement = 3;
+
    this.age ++; 
+   this.hunger += hungerIncrement;
+   this.fitness -= fitnessDecrement;
+}
+
+Pet.prototype.walk = function() {
+    const maximumFitness = 10;
+    const currentFitness = this.fitness;
+    const fitnessImprovement = 4;
+    if (maximumFitness - currentFitness > fitnessImprovement) {
+        this.fitness += fitnessImprovement;
+    }
+    else {
+        this.fitness = maximumFitness;
+    }
 }
 
 module.exports = Pet;
